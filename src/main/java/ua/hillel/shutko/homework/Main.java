7eeea96c0c6af317a71c0b493d33d9c96487c5dc
Shutko Svetlana <svetalysenko25@gmail.com>
2023-11-28 15:45:39 +0200

    added homework method findWordPosition

--- src/main/java/ua/hillel/shutko/homework/Main.java
+++ src/main/java/ua/hillel/shutko/homework/Main.java
@@ -3,16 +3,37 @@ package ua.hillel.shutko.homework;
 public class Main {
     public static void main(String[] args) {
         findSymbolOccurrence("Hello World!", 'l');
+        System.out.println();
+
+        findWordPosition("Apollo", "pollo");
+        System.out.println();
+        findWordPosition("Apple", "Plant");
+        System.out.println();
+        findWordPosition("Pineapple", "apple");
+
     }
 
     static void findSymbolOccurrence(String textString, char symbol) {
-        char[] conversionToSymbol = textString.toCharArray();
+        char[] charArray = textString.toCharArray();
         int counter = 0;
-        for (char c : conversionToSymbol) {
+        for (char c : charArray) {
             if (c == symbol) {
                 counter += 1;
             }
         }
         System.out.println("Number of matching characters " + counter);
     }
+
+    static void findWordPosition(String source, String target) {
+        System.out.println("Source: " + source);
+        System.out.println("Target: " + target);
+        if (source.contains(target)) {
+            int indexElement = source.indexOf(target);
+            System.out.println("Result: " + indexElement);
+        } else {
+            System.out.println("Result: -1");
+        }
+    }
+
+
 }
