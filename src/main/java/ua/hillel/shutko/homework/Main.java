f59596d6fc0c118e0b830680e709807b58a9edbf
Shutko Svetlana <svetalysenko25@gmail.com>
2023-11-29 14:12:05 +0200

    added homework method stringReverse

--- src/main/java/ua/hillel/shutko/homework/Main.java
+++ src/main/java/ua/hillel/shutko/homework/Main.java
@@ -2,7 +2,7 @@ package ua.hillel.shutko.homework;
 
 public class Main {
     public static void main(String[] args) {
-        findSymbolOccurrence("Hello World!", 'l');
+        findSymbolOccurrence("Never look back", 'k');
         System.out.println();
 
         findWordPosition("Apollo", "pollo");
@@ -10,29 +10,51 @@ public class Main {
         findWordPosition("Apple", "Plant");
         System.out.println();
         findWordPosition("Pineapple", "apple");
+        System.out.println();
+
+        stringReverse("Hello");
 
     }
 
-    static void findSymbolOccurrence(String textString, char symbol) {
-        char[] charArray = textString.toCharArray();
+    static void findSymbolOccurrence(String enterTheString, char enterThesymbol) {
+        char[] charArray = enterTheString.toCharArray();
         int counter = 0;
         for (char c : charArray) {
-            if (c == symbol) {
+            if (c == enterThesymbol) {
                 counter += 1;
             }
         }
         System.out.println("Number of matching characters " + counter);
     }
 
+    // method findSymbolOccurrence version two
+//    static void findSymbolOccurrence(String enterTheString, char enterThesymbol) {
+//        int counter = 0;
+//        for (int i = 0; i <= enterTheString.length() - 1; i++) {
+//            if (enterTheString.charAt(i) == enterThesymbol) {
+//                counter++;
+//            }
+//        }
+//        System.out.println("Number of matching characters " + counter);
+//    }
+
     static void findWordPosition(String source, String target) {
         System.out.println("Source: " + source);
         System.out.println("Target: " + target);
         if (source.contains(target)) {
-            int indexElement = source.indexOf(target);
-            System.out.println("Result: " + indexElement);
+            System.out.println("Result: " + source.indexOf(target));
         } else {
             System.out.println("Result: -1");
         }
+    }
+
+    static void stringReverse(String enterTheString) {
+        char[] charArray = enterTheString.toCharArray();
+        String result = "";
+        for (int i = charArray.length - 1; i >= 0; i--) {
+            result += charArray[i];
+        }
+        System.out.println(enterTheString + " -> " + result);
     }
 
 
