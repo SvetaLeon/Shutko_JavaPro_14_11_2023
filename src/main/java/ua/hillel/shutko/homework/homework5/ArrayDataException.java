4592ce1dee6d4c2098163d68065b72ba75a98e3c
Shutko Svetlana <svetalysenko25@gmail.com>
2023-12-08 13:50:53 +0200

    added homework 5

---
+++ src/main/java/ua/hillel/shutko/homework/homework5/ArrayDataException.java
@@ -0,0 +1,7 @@
+package ua.hillel.shutko.homework.homework5;
+
+public class ArrayDataException extends Exception {
+    public ArrayDataException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ua/hillel/shutko/homework/homework5/ArraySizeException.java
@@ -0,0 +1,7 @@
+package ua.hillel.shutko.homework.homework5;
+
+public class ArraySizeException extends Exception {
+    public ArraySizeException(String message) {
+        super(message);
+    }
+}
