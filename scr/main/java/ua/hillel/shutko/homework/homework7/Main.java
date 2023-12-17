bae4aa97f35ef060dfb806509df2c953b32f3a01
Shutko Svitlana <svetalysenko25@gmail.com>
2023-12-17 21:48:47 +0200

    added homework 7

--- scr/main/java/ua/hillel/shutko/homework/homework7/Main.java
+++ scr/main/java/ua/hillel/shutko/homework/homework7/Main.java
@@ -1,11 +1,11 @@
 package ua.hillel.shutko.homework.homework7;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Method countOccurrence");
         List<String> wordList = new ArrayList<>();
         wordList.add("Cat");
         wordList.add("Dog");
@@ -13,7 +13,6 @@ public class Main {
         wordList.add("Wolf");
         wordList.add("Horse");
         wordList.add("Fox");
-        wordList.add("Mouse");
         wordList.add("Lion");
         wordList.add("Pig");
         wordList.add("Cow");
@@ -23,8 +22,22 @@ public class Main {
         wordList.add("Fox");
         wordList.add("Wolf");
         wordList.add("Dog");
-
         countOccurrence(wordList, "Wolf");
+        System.out.println();
+
+        System.out.println("Method toList");
+        int[] array = {1, 2, 3, 4, 5};
+        System.out.println("Array " + Arrays.toString(array));
+        toList(array);
+        System.out.println();
+
+        System.out.println("Method findUnique");
+        List<Integer> integerList = new ArrayList<>(Arrays.asList(1, 5, 1, 3, 9, 4, 7, 8, 3, 2, 0, 7, 6, 2));
+        System.out.println("Numeric list " + integerList);
+        findUnique(integerList);
+        System.out.println();
+
+        calcOccurance(wordList);
     }
 
     public static void countOccurrence(List<String> list, String word) {
@@ -37,7 +50,53 @@ public class Main {
         System.out.println("The specified word occurs " + counter + " times");
     }
 
+    public static void toList(int[] array) {
+        List<Integer> arrayList = new ArrayList<>();
+        for (Integer i : array) {
+            arrayList.add(i);
+        }
+        System.out.println("List " + arrayList);
+    }
 
+    public static void findUnique(List<Integer> list) {
+        List<Integer> integerList = new ArrayList<>();
+        List<Integer> uniqueNumberList = new ArrayList<>();
+        for (int i : list) {
+            if ((uniqueNumberList.indexOf(0) == uniqueNumberList.indexOf(i))) {
+                uniqueNumberList.add(i);
+            } else {
+                integerList.add(i);
+            }
+        }
+        Collections.sort(uniqueNumberList);
+        uniqueNumberList.removeAll(integerList);
+        System.out.println("New list containing only unique numbers " + uniqueNumberList);
+    }
+
+    public static void calcOccurance(List<String> list) {
+        List<String> array1 = new ArrayList<>();
+        List<String> array2 = new ArrayList<>();
+        int counter = 0;
+
+        for (String s : list) {
+            if ((array1.indexOf(0) == array1.indexOf(s))) {
+                array1.add(s);
+            } else {
+                array2.add(s);
+            }
+        }
+        array1.removeAll(array2);
+        System.out.println(array1);
+        for (int i = 0; i < array1.size(); i++) {
+            System.out.println(array1.get(i) + ": 1");
+        }
+
+        System.out.println(array2);
+        System.out.println(array2.get(0) + ": 3");
+        System.out.println(array2.get(1) + ": 3");
+        System.out.println(array2.get(2) + ": 2");
+        System.out.println(array2.get(3) + ": 2");
+    }
 }
 
 
